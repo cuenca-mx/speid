cfc844c2808822d0b07192b2ee598b49597dec33
Matin Tamizi <matin@cuenca.io>
2018-06-18 09:19:46 -0500

    log posts

--- stpmex_handler/views.py
+++ stpmex_handler/views.py
@@ -1,4 +1,8 @@
-from stpmex_handler import app
+from flask import request
+
+from stpmex_handler import app, db
+from stpmex_handler.models import Request
+from stpmex_handler.tables.types import HttpRequestMethod
 
 
 @app.route('/')
@@ -6,6 +10,25 @@ def health_check():
     return "I'm healthy!"
 
 
-@app.route('/stp')
+@app.route('/stp', methods=['POST'])
 def callback_handler():
-    pass
+    return 'got it!'
+
+
+@app.after_request
+def log_posts(response):
+    if request.method == 'POST':
+        path_limit = Request.__table__.c.path.type.length
+        qs_limit = Request.__table__.c.query_string.type.length
+        req = Request(
+            method=HttpRequestMethod(request.method),
+            status_code=response.status_code,
+            path=request.path[:path_limit],
+            query_string=request.query_string.decode()[:qs_limit],
+            ip_address=request.remote_addr,
+            headers=dict(request.headers),
+            body=str(dict(request.form)) or request.data.strip()
+        )
+        db.session.add(req)
+        db.session.commit()
+    return response
