65bee7c5d5ae343797f3bacbe4b4b66c11f7f2cf
Ricardo Sanchez <ricardo_8990@hotmail.com>
2018-10-02 22:02:04 -0500

    Adding support for ampq URL connection types

--- speid/rabbit/__init__.py
+++ speid/rabbit/__init__.py
@@ -2,5 +2,10 @@ import os
 import pika
 
 RABBIT_URL = os.getenv('AMPQ_ADDRESS')
-CONNECTION = pika.BlockingConnection(pika.ConnectionParameters(
-    host=RABBIT_URL))
+RABBIT_URL_TYPE = os.getenv('AMPQ_ADDRESS_TYPE')
+
+if RABBIT_URL_TYPE == 'url':
+    parameters = pika.URLParameters(RABBIT_URL)
+else:
+    parameters = pika.ConnectionParameters(host=RABBIT_URL)
+CONNECTION = pika.BlockingConnection(parameters)
