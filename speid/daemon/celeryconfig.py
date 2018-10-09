433f1a08846eaf457d6e3ccf3c9d7f3266003363
Ricardo Sanchez <ricardo_8990@hotmail.com>
2018-10-09 16:38:56 -0500

    Send results via Celery

--- speid/daemon/celeryconfig.py
+++ speid/daemon/celeryconfig.py
@@ -1,9 +1,10 @@
 import os
 
-from speid.rabbit import NEW_ORDER_QUEUE
+from speid.queue import NEW_ORDER_QUEUE
 
 broker_url = os.environ['AMPQ_ADDRESS']
 task_serializer = 'json'
 accept_content = ['json']
 task_default_queue = NEW_ORDER_QUEUE
 include = ['speid.daemon.tasks']
+backend = 'amqp'
