dc4fb2a0c10e7d316e668c17abd76bd90333549c
Ricardo Sanchez <ricardo_8990@hotmail.com>
2018-10-10 12:39:43 -0500

    Add Elasticsearch, kibana and Logstash to docker compose.

--- speid/daemon/__init__.py
+++ speid/daemon/__init__.py
@@ -3,8 +3,8 @@ import os
 
 import logstash
 
-elastic_host = os.environ['ELASTIC_HOST']
-elastic_port = os.environ['ELASTIC_PORT']
+logstash_host = os.environ['ELASTIC_HOST']
+logstash_port = os.environ['ELASTIC_PORT']
 log_level = os.environ['CELERY_LOG_LEVEL']
 if log_level == 'debug':
     level = logging.DEBUG
@@ -14,7 +14,7 @@ else:
 
 def initialize_logstash(logger=None, loglevel=level, **kwargs):
     handler = logstash.TCPLogstashHandler(
-        elastic_host, elastic_port, tags=['celery'],
+        logstash_host, logstash_port, tags=['celery'],
         message_type='celery', version=1
     )
     handler.setLevel(loglevel)
