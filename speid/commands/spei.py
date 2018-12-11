c5220f33c19d242a61e1aa090badda64f3499823
Pach <yohopa@hotmail.com>
2018-12-11 15:29:06 -0600

    Spring 3 11 12 (#23)
    * Validate previous transaction #20
    
    * Script #20
    
    * Python style f t world
    
    * Import pdb
    
    * Send correct AccountType
    
    * Changes required

---
+++ speid/commands/spei.py
@@ -0,0 +1,70 @@
+import click
+import os
+
+from ast import literal_eval
+import stpmex
+
+from speid import db
+from speid.models import Event
+from speid.models import Transaction
+from speid.tables.types import State
+from speid.daemon.tasks import execute_task
+
+
+@click.command()
+@click.option('--speid_id', default=None, help='Specific speid id to execute')
+def re_execute_transactions(speid_id):
+    """Retry send a transaction to STP, it takes de values
+    of the event created before
+    """
+    stp_private_location = os.environ['STP_PRIVATE_LOCATION']
+    wsdl_path = os.environ['STP_WSDL']
+    stp_empresa = os.environ['STP_EMPRESA']
+    priv_key_passphrase = os.environ['STP_KEY_PASSPHRASE']
+    stp_prefijo = os.environ['STP_PREFIJO']
+
+    with open(stp_private_location) as fp:
+        private_key = fp.read()
+    stpmex.configure(wsdl_path=wsdl_path,
+                     empresa=stp_empresa,
+                     priv_key=private_key,
+                     priv_key_passphrase=priv_key_passphrase,
+                     prefijo=int(stp_prefijo))
+    if speid_id is None:
+        transactions = (db.session.query(Transaction)
+                        .filter_by(estado='submitted',
+                                   orden_id=None))
+        for transaction in transactions:
+            send_queue(transaction)
+    else:
+        transaction = (db.session.query(Transaction)
+                       .filter_by(speid_id=speid_id,
+                                  estado='submitted',
+                                  orden_id=None)
+                       .first())
+        send_queue(transaction)
+
+
+def send_queue(transaction):
+    try:
+        event = (db.session.query(Event).
+                 filter_by(
+            transaction_id=transaction.id,
+            type=State.created
+        ).order_by(Event.created_at.desc()).first())
+
+        event_retry = Event(
+            transaction_id=transaction.id,
+            type=State.retry,
+            meta=event.meta
+        )
+        db.session.add(event_retry)
+        db.session.commit()
+        order_val = literal_eval(event.meta)
+        execute_task(order_val)
+    except Exception as exc:
+        print(exc)
+
+
+if __name__ == "__main__":
+    re_execute_transactions()
--- speid/daemon/tasks.py
+++ speid/daemon/tasks.py
@@ -5,6 +5,7 @@ from sentry_sdk.integrations.celery import CeleryIntegration
 
 from speid import db
 from speid.models import Event
+from speid.models import Transaction
 from speid.models.exceptions import MalformedOrderException
 from speid.models.transaction import TransactionFactory
 from speid.helpers import callback_helper
@@ -25,7 +26,9 @@ def retry_timeout(attempts):
 def send_order(self, order_val):
     try:
         execute_task(order_val)
-    except ConnectionError as exc:
+    except MalformedOrderException as malformed_exception:
+        raise malformed_exception
+    except Exception as exc:
         self.retry(countdown=retry_timeout(self.request.retries), exc=exc)
 
 
@@ -44,19 +47,14 @@ def execute(order_val):
         type=State.created,
         meta=str(order_val)
     )
-
     try:
         # Get version
         version = 0
         if "version" in order_val:
             version = order_val['version']
-
-        # Recover orden
+            # Recover orden
         transaction, order = \
             TransactionFactory.create_transaction(version, order_val)
-        # Save transaction
-        db.session.add(transaction)
-        db.session.commit()
 
         if transaction.estado == Estado.error:
             if transaction.speid_id is not None:
@@ -69,12 +67,30 @@ def execute(order_val):
                     )
                 )
             raise MalformedOrderException()
+        previous_transaction = None
+        # Review if there is another transaction
+        if version != 0:
+            previous_transaction = (
+                db.session.query(Transaction).filter
+                (Transaction.speid_id == order_val['speid_id'])
+                .first())
+
+        if previous_transaction is None:
+            # Save transaction
+            db.session.add(transaction)
+            db.session.commit()
+        else:
+            event_created.type = State.retry
+            transaction.id = previous_transaction.id
+            order.clave_rastreo = previous_transaction.clave_rastreo
 
         event_created.transaction_id = transaction.id
 
         # Send order to STP
         order.monto = order.monto / 100
+
         res = order.registra()
+
     finally:
         db.session.add(event_created)
         db.session.commit()
