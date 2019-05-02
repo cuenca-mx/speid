509845e87e450327bffaa957a7c01c1358e25673
Jose Gregorio Castro Lazo <josegregorio994@gmail.com>
2019-05-02 16:17:20 -0500

    recon test

--- test/test_recon.py
+++ test/test_recon.py
@@ -11,14 +11,7 @@ class TestRecon:
         with open('/tmp/report.txt', 'w') as f:
             f.write(file_recon)
         recon_transactions()
-        # transaction = (
-        #     db.session.query(Transaction).filter_by(orden_id=22673742).first()
-        # )
-        # assert transaction
         transaction = (
-            db.session.query(Transaction)
+            db.session.query(Transaction).filter_by(orden_id=22673742).first()
         )
-        arr = []
-        for trans in transaction.all():
-            arr.append(trans.orden_id)
-        assert arr == 'testing'
+        assert transaction
