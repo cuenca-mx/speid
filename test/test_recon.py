f5a5a96ce08611b17422757d4eabc1930d06c73c
Jose Gregorio Castro Lazo <josegregorio994@gmail.com>
2019-05-09 15:29:42 -0500

    change status failed to enum

--- test/test_recon.py
+++ test/test_recon.py
@@ -4,6 +4,7 @@ from test.custom_vcr import my_vcr
 from speid import db
 from speid.models import Transaction
 from speid.recon import recon_transactions
+from speid.tables.types import Estado
 
 
 logging.basicConfig()
@@ -14,7 +15,7 @@ vcr_log.setLevel(logging.DEBUG)
 
 class TestRecon:
     @my_vcr.use_cassette('test/cassettes/test_recon.yaml')
-    def test_reconciliate(self, file_recon):
+    def test_reconciliate_status_failed(self, file_recon):
         with open('/tmp/report.txt', 'w') as f:
             f.write(file_recon)
         recon_transactions()
@@ -22,13 +23,13 @@ class TestRecon:
             db.session.query(Transaction).filter_by(
                 orden_id=22673742,
                 clave_rastreo='CR1547453521',
-                estado='failed'
+                estado=Estado.failed
             ).first()
         )
         assert transaction
 
     @my_vcr.use_cassette('test/cassettes/test_recon1.yaml')
-    def test_reconciliate_status_failed(self, file_recon1):
+    def test_reconciliate_succeeded(self, file_recon1):
         with open('/tmp/report.txt', 'w') as f:
             f.write(file_recon1)
         recon_transactions()
