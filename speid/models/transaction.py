a62df01b2de553e478ff7b30fd1e042618b154fc
Matin Tamizi <matin@cuenca.com>
2019-10-02 08:33:58 -0500

    oops again!

--- speid/models/transaction.py
+++ speid/models/transaction.py
@@ -129,7 +129,7 @@ class Transaction(Document):
         # remove if value is None
         for k, v in optionals.items():
             if v is None:
-                optionals.pop(v)
+                optionals.pop(k)
 
         order = Orden(
             monto=self.monto / 100.0,
