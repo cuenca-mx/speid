c7df488e934a4d5f0d7a6e31d43de854d7c8767a
Yisus <alan@cuenca.com>
2020-05-18 15:01:51 -0500

    Fix del fix

--- speid/validations/speid_transaction.py
+++ speid/validations/speid_transaction.py
@@ -1,3 +1,5 @@
+from typing import Optional
+
 from pydantic import StrictStr
 from pydantic.dataclasses import dataclass
 from stpmex.types import TipoCuenta
@@ -18,27 +20,27 @@ class SpeidTransaction:
     rfc_curp_ordenante: StrictStr
     speid_id: StrictStr
     version: str
-    empresa: str = ''
-    folio_origen: str = ''
-    clave_rastreo: str = ''
+    empresa: Optional[str] = None
+    folio_origen: Optional[str] = None
+    clave_rastreo: Optional[str] = None
     tipo_pago: int = 1
-    tipo_cuenta_ordenante: str = ''
+    tipo_cuenta_ordenante: Optional[str] = None
     tipo_cuenta_beneficiario: int = 40
     rfc_curp_beneficiario: str = "ND"
-    email_beneficiario: str = ''
-    tipo_cuenta_beneficiario2: str = ''
-    nombre_beneficiario2: str = ''
-    cuenta_beneficiario2: str = ''
-    rfc_curpBeneficiario2: str = ''
-    concepto_pago2: str = ''
-    clave_cat_usuario1: str = ''
-    clave_cat_usuario2: str = ''
-    clave_pago: str = ''
-    referencia_cobranza: str = ''
-    referencia_numerica: int = 0
-    tipo_operacion: str = ''
+    email_beneficiario: Optional[str] = None
+    tipo_cuenta_beneficiario2: Optional[str] = None
+    nombre_beneficiario2: Optional[str] = None
+    cuenta_beneficiario2: Optional[str] = None
+    rfc_curpBeneficiario2: Optional[str] = None
+    concepto_pago2: Optional[str] = None
+    clave_cat_usuario1: Optional[str] = None
+    clave_cat_usuario2: Optional[str] = None
+    clave_pago: Optional[str] = None
+    referencia_cobranza: Optional[str] = None
+    referencia_numerica: Optional[int] = None
+    tipo_operacion: Optional[str] = None
     topologia: str = "T"
-    usuario: str = ''
+    usuario: Optional[str] = None
     medio_entrega: int = 3
     prioridad: int = 1
 
