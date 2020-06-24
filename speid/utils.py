c82e64d8f5dbcd9c6a738f1c407b0012221ede73
Ricardo Sánchez <rsc@cuenca.com>
2020-06-24 16:04:41 -0500

    Remove unused code

--- speid/utils.py
+++ speid/utils.py
@@ -2,37 +2,7 @@ from functools import wraps
 
 from flask import jsonify, make_response
 
-from speid import app
-
-
-def get(rule: str, **options):
-    def decorator(view):
-        @wraps(view)
-        def decorated(*args, **kwargs):
-            status_code, body = view(*args, **kwargs)
-            return make_response(jsonify(body), status_code)
-
-        endpoint = options.pop('endpoint', None)
-        app.add_url_rule(rule, endpoint, decorated, methods=['GET'], **options)
-        return view
-
-    return decorator
-
-
-def patch(rule: str, **options):
-    def decorator(view):
-        @wraps(view)
-        def decorated(*args, **kwargs):
-            status_code, body = view(*args, **kwargs)
-            return make_response(jsonify(body), status_code)
-
-        endpoint = options.pop('endpoint', None)
-        app.add_url_rule(
-            rule, endpoint, decorated, methods=['PATCH'], **options
-        )
-        return view
-
-    return decorator
+from . import app
 
 
 def post(rule: str, **options):
