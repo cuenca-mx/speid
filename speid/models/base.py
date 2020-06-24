7597d2a8fa3c570c8d6cb59f05324b55935d6df0
Ricardo Sánchez <rsc@cuenca.com>
2020-06-24 16:12:19 -0500

    Remove unused code

--- speid/models/base.py
+++ speid/models/base.py
@@ -6,6 +6,3 @@ from speid.models.helpers import mongo_to_dict
 class BaseModel:
     def to_dict(self) -> Union[dict, None]:
         return mongo_to_dict(self, [])
-
-    def __repr__(self):
-        return str(self.to_dict())
