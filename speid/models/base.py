2b1a12f6ad707e436ea65f2d23ed32cf3ef1f7df
Ricardo Sánchez <rsc@cuenca.com>
2020-06-24 19:58:20 -0500

    Tests para helpers

--- speid/models/base.py
+++ speid/models/base.py
@@ -4,5 +4,5 @@ from speid.models.helpers import mongo_to_dict
 
 
 class BaseModel:
-    def to_dict(self) -> Union[dict, None]:
-        return mongo_to_dict(self, [])
+    def to_dict(self, exclude_fields: list = []) -> Union[dict, None]:
+        return mongo_to_dict(self, exclude_fields)
