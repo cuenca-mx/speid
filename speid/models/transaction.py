885c8deba4e8fe14bb16a5a21f35e02f00b10eb3
Felipe López <flh.1989@gmail.com>
2022-04-06 12:37:21 -0500

    using Account to retrieve moral or physical

--- speid/models/transaction.py
+++ speid/models/transaction.py
@@ -142,7 +142,7 @@ class Transaction(Document, BaseModel):
         # Validate account has already been created
         if not SKIP_VALIDATION_PRIOR_SEND_ORDER:
             try:
-                account = PhysicalAccount.objects.get(
+                account = Account.objects.get(
                     cuenta=self.cuenta_ordenante
                 )
                 assert account.estado is Estado.succeeded
