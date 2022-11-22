1f6a4795d64d14a35f69ece7f4d57340bfe58478
Pach <arturo@cuenca.com>
2022-11-22 10:19:55 -0600

    Update transaction.py

--- speid/models/transaction.py
+++ speid/models/transaction.py
@@ -59,7 +59,7 @@ def pre_save_transaction(sender, document):
 
 
 # Min amount accepted in restricted accounts
-MIN_AMOUNT = int(os.getenv('MIN_AMOUNT','10000'))
+MIN_AMOUNT = int(os.getenv('MIN_AMOUNT', '10000'))
 
 
 @updated_at.apply
