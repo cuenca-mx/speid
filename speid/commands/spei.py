548f3ab2bc63e7823c862c7e5f8eebbb169ab4b1
AlexViquez <alexviquez22@gmail.com>
2023-08-28 10:43:35 -0600

    fix app cli group

--- speid/commands/spei.py
+++ speid/commands/spei.py
@@ -1,12 +1,12 @@
 import click
 from mongoengine import DoesNotExist
-
+from speid import app
 from speid.helpers.callback_helper import set_status_transaction
 from speid.models import Event, Transaction
 from speid.types import Estado, EventType
 
 
-@click.group('speid')
+@app.cli.group('speid')
 def speid_group():
     """Perform speid actions."""
 
